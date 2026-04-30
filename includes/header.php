321b5c7b9be7c237893560b4aa81df6403e5ec82
Gorka <gorkakortabitarte@gmail.com>
2026-04-30 23:32:26 +0200

    Estructura inicial del proyecto

---
+++ includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <title>Document</title>
+</head>
+
+<header class="main-header">
+    <div class="header-container">
+        <div class="logo">
+            <a href="../index.php">
+                <img src="../assets/img/logo.png" alt="Logo de Errotari">
+            </a>
+        </div>
+
+        <nav class="main-nav">
+            <ul>
+                <li><a href="/index.php" class="active">Home</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Micologia</a></li>
+                <li><a href="#">Revista</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
